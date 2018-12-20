2c15da929f021294b4f484a1d7fb56842a88a479
gaoyphome <1039524560@qq.com>
2018-12-20 18:19:34 +0800

    spring security

---
+++ oa_service/src/main/java/com/sxdx/oa_service/config/SecurityConfiguration.java
@@ -0,0 +1,8 @@
+package com.sxdx.oa_service.config;
+
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+}
