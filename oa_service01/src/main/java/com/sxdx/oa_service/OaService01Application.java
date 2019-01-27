faa687fdbbb61bc15add0ec053811aab47953d80
gaoyipeng <garnett_boy@sina.com>
2019-01-27 18:03:32 +0800

    ribbon 微服务互相调用，ribbon自定义负载均衡算法

---
+++ oa_service01/src/main/java/com/sxdx/oa_service/OaService01Application.java
@@ -0,0 +1,14 @@
+package com.sxdx.oa_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OaService01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OaService01Application.class, args);
+    }
+
+}
+
