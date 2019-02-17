b308c47c8071727739431bcf1cb22391162ccf38
gaoyipeng <garnett_boy@sina.com>
2019-02-17 17:35:31 +0800

    添加zuul服务熔断功能

---
+++ oa_zuul/src/main/java/com/sxdx/oa_zuul/config/MyZuulFallbackProvider.java
@@ -0,0 +1,74 @@
+package com.sxdx.oa_zuul.config;
+
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Zuul上配置路由熔断
+ */
+@Component
+public class MyZuulFallbackProvider implements FallbackProvider {
+    /**
+     * 用于指定熔断功能应用于哪个路由服务;
+     * @return
+     */
+    @Override
+    public String getRoute() {
+        //return "oa-service";
+        return "*";  //实现对所有的路由服务的熔断
+    }
+
+
+
+    public ClientHttpResponse fallbackResponse() {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return 200;
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return "OK";
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream("The service is unavailable.".getBytes());
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                return headers;
+            }
+        };
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
+        if (cause != null && cause.getCause() != null) {
+            String reason = cause.getCause().getMessage();
+        }
+        return fallbackResponse();
+    }
+}
